248738ac1b2438ff6c6103b8d93f0e274a6c7548
Gordon Hall <gordon@gordonwritescode.com>
2015-04-23 17:22:53 -0400

    add placeholders from main public api

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 /**
 * @module libmuttr
 */
+
+'use strict';
+
+var Client = require('./lib/client');
+var DHT = require('./lib/dht');
+var Identity = require('./lib/identity');
+
+/**
+* Creates and returns a new muttr.Client
+* #createClient
+* @param {object} options
+*/
+exports.createClient = function(options) {
+  return new Client(options);
+};
+
+/**
+* Creates and returns a new muttr.DHT
+* #createConnection
+* @param {object} options
+*/
+exports.createConnection = function(options) {
+  return new DHT(options);
+};
+
+/**
+* Creates and returns a new muttr.Identity
+* #createIdentity
+* @param {object} options
+*/
+exports.createIdentity = function(options) {
+  return new Identity(options);
+};
