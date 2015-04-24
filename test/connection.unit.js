2292399af2f3ffefa5cc84c88742f33f53c4f5de
Gordon Hall <gordon@gordonwritescode.com>
2015-04-24 16:46:52 -0400

    implemented connection class, added placeholder for tests

---
+++ test/connection.unit.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var sinon = require('sinon');
+var expect = require('chai').expect;
+var Connection = require('../lib/connection');
+
+describe('Connection', function() {
+
+  describe('@constructor', function() {
+
+
+
+  });
+
+  describe('#validateMessageKey', function() {
+
+
+
+  });
+
+  describe('#get', function() {
+
+
+
+  });
+
+  describe('#put', function() {
+
+
+
+  });
+
+  describe('#open', function() {
+
+
+
+  });
+
+  describe('#_forwardPort', function() {
+
+
+
+  });
+
+  describe('#_joinNetwork', function() {
+
+
+
+  });
+
+});
