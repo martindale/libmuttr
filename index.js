15ae0ed4fe37605d658a2b178419a07e0d9713d8
Gordon Hall <gordon@gordonwritescode.com>
2015-04-28 18:44:49 -0400

    started working on client

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 'use strict';
 
 exports.Client = require('./lib/client');
-exports.Connection = require('./lib/dht');
+exports.Connection = require('./lib/connection');
 exports.Identity = require('./lib/identity');
 exports.Session = require('./lib/session');
 exports.utils = require('./lib/utils');
